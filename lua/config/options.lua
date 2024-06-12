9ce48446ba13f4d8de0a93dd0c04699e2e55040a
Michael Landes <mlandes@logiconix.com>
2024-06-12 13:44:10 -0700

    tiny ui options tweaks

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,6 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
+-- options are automatically loaded before lazy.nvim startup
+-- default options that are always set: https://github.com/lazyvim/lazyvim/blob/main/lua/lazyvim/config/options.lua
+-- add any additional options here
+
+vim.opt.clipboard:remove("unnamedplus")
+vim.opt.colorcolumn = "120"
