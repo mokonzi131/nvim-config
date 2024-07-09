38d6889c9deddb2634d3771150304b57f8aa2b4a
Michael Landes <mlandes@logiconix.com>
2024-07-09 00:36:19 -0700

    C++ support, minor UI improvements

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,29 +1,5 @@
 return {
-  -- {
-  --   "nvim-treesitter/nvim-treesitter",
-  --   opts = {
-  --     ensure_installed = {
-  --       "bash",
-  --       "html",
-  --       "javascript",
-  --       "json",
-  --       "lua",
-  --       "markdown",
-  --       "markdown_inline",
-  --       "python",
-  --       "query",
-  --       "regex",
-  --       "tsx",
-  --       "typescript",
-  --       "vim",
-  --       "yaml",
-  --     },
-  --   },
-  -- },
-  --
-
-  -- since `vim.tbl_deep_extend`, can only merge tables and not lists, the code above
-  -- would overwrite `ensure_installed` with the new value.
+  -- since `vim.tbl_deep_extend`, can only merge tables and not lists, just setting opts replaces them
   -- If you'd rather extend the default config, use the code below instead:
   {
     "nvim-treesitter/nvim-treesitter",
@@ -32,6 +8,18 @@ return {
       vim.list_extend(opts.ensure_installed, {
         "c",
         "cpp",
+        "css",
+        "glsl",
+        "hlsl",
+        "html",
+        "javascript",
+        "json",
+        "jsonc",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "meson",
+        "python",
       })
     end,
   },
